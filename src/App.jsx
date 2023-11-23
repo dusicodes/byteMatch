f6b9204043e401b85b07763e51e05707ffad8a7b
Endurance Aloja <alojaeddie@gmail.com>
2023-11-23 01:17:37 +0000

    test commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+import "./App.css";
+import HomePage from "./components/homePage";
+
+function App() {
+  return (
+    <>
+      <HomePage />
+    </>
+  );
+}
+
+export default App;
